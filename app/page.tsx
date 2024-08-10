3afd177c42f8b7b0bf5016af431dbd65666bf2c7
bealecs <cliftonscottbeale@gmail.com>
2024-08-10 16:20:44 -0500

    added sections with cards for each sport on landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Header from "./components/Header";
+import Hero from "./components/hero/Hero";
 
 export default function Home() {
   return (
     <main>
       <Header />
+      <Hero />
     </main>
   );
 }
